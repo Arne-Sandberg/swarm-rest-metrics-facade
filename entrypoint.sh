a108f8f7ed1ded1f75e4c248deb9bb94210bede2
Tom Saleeba <tom.saleeba@gmail.com>
2018-11-25 17:46:40 +1030

    fix: allow connections from public web

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ mitmdump \
   -p $LISTEN_PORT \
   --mode reverse:$TARGET_URL \
   -s ./log-metrics.py \
-  --set termlog_verbosity=$LOG_LEVEL
+  --set termlog_verbosity=$LOG_LEVEL \
+  --set block_global=false
